69cff7cc33dcd10f38d7585efb284a8bafb4ef23
mthorry <mthorry@gmail.com>
2017-07-23 10:48:35 -0400

    done

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,72 @@
+# AN owner will know about all its pets, be able to buy a pet, set the name of a pet (which the pet can't change), change a pet's mood though walking, feed,playing, and sell all of its pets
+require "pry"
+
 class Owner
-  # code goes here
+
+  attr_accessor :name, :mood, :pets
+  attr_reader :species
+
+  @@all = []
+
+  def initialize(species)
+    @pets = {fishes: [], cats: [], dogs: []}
+    @species = species
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def say_species
+    "I am a #{self.species}."
+  end
+
+  def walk_dogs
+    @pets[:dogs].each {|dog| dog.mood = "happy"}
+  end
+
+  def play_with_cats
+    @pets[:cats].each {|cat| cat.mood = "happy"}
+  end
+
+  def feed_fish
+    @pets[:fishes].each {|fish| fish.mood = "happy" }
+  end
+
+  def sell_pets
+    @pets[:dogs].each {|dog| dog.mood = "nervous"}
+    @pets[:cats].each {|cat| cat.mood = "nervous"}
+    @pets[:fishes].each {|fish| fish.mood = "nervous"}
+    @pets.clear
+    # binding.pry
+  end
+
+  def list_pets
+    "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
+    # binding.pry
+  end
+
+  def buy_cat(name)
+    new_cat = Cat.new(name)
+    @pets[:cats] << new_cat
+  end
+
+  def buy_dog(name)
+    new_dog = Dog.new(name)
+    @pets[:dogs] << new_dog
+  end
+
+  def buy_fish(name)
+    new_fish = Fish.new(name)
+    @pets[:fishes] << new_fish
+  end
 end
